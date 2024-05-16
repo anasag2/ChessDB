12bf1be977ed04b01a86aed6c8f9b5a628885693
anasag2 <anas_02.sha@hotmail.com>
2024-05-16 13:28:50 +0300

    initial commit

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,8 +1,27 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import {Slot, Stack} from 'expo-router';
+import {useFonts} from 'expo-font';
 
 const RootLayout = () => {
+
+  const [loaded] = useFonts({
+    "IBM-Plex-Sans": require("../assets/fonts/IBMPlexSans-Regular.ttf"),
+    "IBM-Plex-Sans-Bold": require("../assets/fonts/IBMPlexSans-Bold.ttf"),
+    "IBM-Plex-Sans-Medium": require("../assets/fonts/IBMPlexSans-Medium.ttf"),
+    "IBM-Plex-Sans-SemiBold": require("../assets/fonts/IBMPlexSans-SemiBold.ttf"),
+    "IBM-Plex-Sans-Light": require("../assets/fonts/IBMPlexSans-Light.ttf"),
+    "IBM-Plex-Sans-ExtraLight": require("../assets/fonts/IBMPlexSans-ExtraLight.ttf"),
+    "IBM-Plex-Sans-Thin": require("../assets/fonts/IBMPlexSans-Thin.ttf"),
+    "IBM-Plex-SemiBoldItalic": require("../assets/fonts/IBMPlexSans-SemiBoldItalic.ttf"),
+    "IBM-Plex-Sans-ThinItalic": require("../assets/fonts/IBMPlexSans-ThinItalic.ttf"),
+    "IBM-Plex-Sans-LightItalic": require("../assets/fonts/IBMPlexSans-LightItalic.ttf"),
+    "IBM-Plex-Sans-ExtraLightItalic": require("../assets/fonts/IBMPlexSans-ExtraLightItalic.ttf"),
+    "IBM-Plex-Sans-MediumItalic": require("../assets/fonts/IBMPlexSans-MediumItalic.ttf"),
+    "IBM-Plex-Sans-BoldItalic": require("../assets/fonts/IBMPlexSans-BoldItalic.ttf")
+  });
+
+
   return (
     <Stack>
         <Stack.Screen name="index"
--- app/index.jsx
+++ app/index.jsx
@@ -1,23 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import {Link} from 'expo-router';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>The Chess DB</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text classname="test-3xl">The Chess DB!</Text>
+      <StatusBar style="auto"/>
       <Link href="/profile" style={{color: 'blue'}}>Profile
       </Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
